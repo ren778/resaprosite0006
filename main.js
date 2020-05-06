0db43bc5352e9d95b5af642b9ddf7a86e5e7713a
ren(蓮) <63449550+ren778@users.noreply.github.com>
2020-05-06 16:44:18 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function runjs() {
+  alert("Hello to JavaScript");
+  if (confirm("yes no?")) {
+    alert("yes!");
+  } else {
+    alert("no...");
+  }
+  if (prompt("type->gplay") == "gplay") {
+    alert("yes!");
+  } else {
+    alert("no...");
+  }
+}
